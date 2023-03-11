0c9f830efaf4cecf4523a457fddf489406bff137
Stephen Mooney <stephen.peter.mooney@gmail.com>
2023-03-11 22:47:51 +0000

    conditional rendering fix

--- src/services/trackingService.js
+++ src/services/trackingService.js
@@ -18,12 +18,25 @@ export default class trackingService{
         const endDate = new Date().toISOString().slice(0, 10);
         const startDate = new Date(Date.now() - 12 * 24 * 60 * 60 * 1000).toISOString().slice(0, 10);
 
+        console.log(endDate);
+        console.log(startDate);
         const options = {
             method: 'GET',
             url: process.env.REACT_APP_GET_GAMES,
             params: {
                 page: [0],
-                dates: [[endDate], [startDate]],
+                dates: ['2023-03-11T00:00:00.000Z',
+                    '2023-03-10T00:00:00.000Z',
+                    '2023-03-09T00:00:00.000Z',
+                    '2023-03-08T00:00:00.000Z',
+                    '2023-03-07T00:00:00.000Z',
+                    '2023-03-06T00:00:00.000Z',
+                    '2023-03-05T00:00:00.000Z',
+                    '2023-03-04T00:00:00.000Z',
+                    '2023-03-03T00:00:00.000Z',
+                    '2023-03-02T00:00:00.000Z',
+                    '2023-03-01T00:00:00.000Z',
+                    '2023-02-28T00:00:00.000Z'],
                 per_page: 12,
                 team_ids: [team.id]
             },
